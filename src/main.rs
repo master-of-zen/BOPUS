a1f2d8876b05199e98c8cf262d244a0eb1e88e86
Zen <true.grenight@gmail.com>
2021-01-19 15:46:16 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,120 @@ extern crate execute;
 use std::env;
 use clap::{App, Arg};
 use std::fs;
+use std::path::Path;
 use std::process::Command;
 use execute::Execute;
+
+fn main() {
+
+    // check is executables exist
+    if is_program_in_path("ffmpeg"){}
+    else {println!("No.");}
+
+    if is_program_in_path("visqol"){}
+    else {println!("No.");}
+
+    // arg parsing
+    let _matches = App::new("Bopus")
+        .version("0.1")
+        .author("Zen <true.grenight@gmail.com>>")
+        .about("Opus bitrate optimizer")
+        .arg(Arg::with_name("INPUT")
+            .short("i")
+            .long("input")
+            .value_name("INPUT")
+            .help("Sets the input file to use")
+            .required(true)
+            .takes_value(true))
+        .arg(Arg::with_name("TARGET")
+            .short("t")
+            .long("target")
+            .help("Sets value of quality to target")
+            .takes_value(true))
+        .get_matches();
+
+    // creating temp dir/removing old
+    let temp_path = Path::new("temp");
+
+    if temp_path.exists(){
+        fs::remove_dir_all(temp_path).ok();
+    }else {
+        fs::create_dir_all("temp").ok().expect("Can't create a temp folder");
+    }
+
+    let input_file: &str = _matches.value_of("INPUT").unwrap();
+    let target_quality: f32 = _matches.value_of("TARGET").unwrap_or("4.3").parse().unwrap();
+
+    // printing some stuff
+    println!("Using input file {}", input_file);
+    println!("Using target quality {}", target_quality);
+
+
+    // making wav
+    make_wav(input_file: &str);
+
+
+    // get metric score
+    let mut bitrate: u32 = 96;
+    let mut count: u32 = 0;
+
+    let mut bitrates: Vec<(u32, f32)> = vec![];
+    // bitrate | score
+
+
+    // Search loop
+    loop {
+        count += 1;
+
+        if count > 6{
+            println!("Get more than {}, ending comparison", count );
+            break
+        }
+
+        println!("Try: {} Bitrate: {}", count, bitrate);
+
+        if count == 1{
+            let score:f32 = make_probe(bitrate);
+        }
+        break
+    }
+}
+
+fn make_probe(bitrate:u32) -> f32{
+    let viqol_score:f32 = 0.9;
+
+    // Audio to opus
+    let mut cmd = Command::new("ffmpeg");
+    cmd.args(&["-y", "-i", "temp/ref.wav", "-c:a","libopus", "-b:a", &format!("{}K", &bitrate.to_string()), &format!("temp/{}.opus", bitrate) ]);
+
+    if let Some(exit_code) = cmd.execute().unwrap() {
+        if exit_code == 0 {}
+        else
+        {
+            eprintln!("Failed");
+            println!("{:?}", cmd.output().unwrap());
+        }
+    }
+    else {eprintln!("Interupted")}
+
+    // Audio to wav
+    let mut cmd = Command::new("ffmpeg");
+    cmd.args(&["-y", "-i", &format!("temp/{}", bitrate), "-ar", "48000", &format!("temp/{}.wav", bitrate)]);
+
+    if let Some(exit_code) = cmd.execute().unwrap() {
+        if exit_code == 0 {}
+        else
+        {
+            eprintln!("Failed");
+            println!("{:?}", cmd.output().unwrap());
+        }
+    }
+    else {eprintln!("Interupted")}
+
+    println!("Calculating Score...");
+
+    viqol_score
+}
 
 fn is_program_in_path(program: &str) -> bool {
     // Check is program in path
@@ -35,9 +147,10 @@ fn make_wav(input: &str){
         else {eprintln!("Interupted")}
 }
 
-fn encode_audio( bitrate: u32 ){
+fn encode_audio(bitrate:u32){
+    // final encode of the audio
     let mut cmd = Command::new("ffmpeg");
-    cmd.args(&["-y", "-i", "temp/ref.wav", "-c:a","libopus", "-b:a", &bitrate.to_string(),  "temp/ref.wav"]);
+    cmd.args(&[ "-y", "-i", "temp/ref.wav", "-c:a","libopus", "-b:a", &format!("{}K", &bitrate.to_string()), &format!("temp/{}", bitrate) ]);
 
     if let Some(exit_code) = cmd.execute().unwrap() {
         if exit_code == 0{println!("Converted to wav");}
@@ -50,65 +163,4 @@ fn encode_audio( bitrate: u32 ){
     else {eprintln!("Interupted")}
 }
 
-fn main() {
-
-
-    // check is executables exist
-    if is_program_in_path("ffmpeg"){}
-    else {println!("No.");}
-
-    if is_program_in_path(""){}
-    else {
-        println!("No.");
-    }
-
-    // arg parsing
-    let _matches = App::new("My Super Program")
-                            .version("0.1")
-                            .author("Zen <true.grenight@gmail.com>>")
-                            .about("Opus bitrate optimizer")
-                            .arg(Arg::with_name("INPUT")
-                                .short("i")
-                                .long("input")
-                                .value_name("INPUT")
-                                .help("Sets the input file to use")
-                                .required(true)
-                                .takes_value(true))
-                            .arg(Arg::with_name("TARGET")
-                                .short("t")
-                                .long("target")
-                                .help("Sets value of quality to target")
-                                .takes_value(true))
-                            .get_matches();
-
-    // creating temp dir
-    fs::create_dir_all("temp").ok().expect("Can't create a temp folder");
-    let input_file: &str = _matches.value_of("INPUT").unwrap();
-    let target_quality: f32 = _matches.value_of("TARGET").unwrap_or("4.3").parse().unwrap();
-
-    // printing some stuff
-    println!("Using input file {}", input_file);
-    println!("Using target quality {}", target_quality);
-
-
-    // making wav
-    make_wav(input_file: &str);
-
-
-    // get metric score
-    let bitrate: u32 = 96;
-    let mut count: u32 = 0;
-
-    loop {
-        count += 1;
-        println!("Try: {} Bitrate: {}", count, bitrate);
-
-        if count == 1{
-
-        }
-        break
-    }
-
-
-}
 
