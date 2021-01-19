be40ca59c17e797aaecc66bee7c306ec044a40a6
Zen <true.grenight@gmail.com>
2021-01-19 17:08:30 +0200

    probing done

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 #![feature(type_ascription)]
 extern crate execute;
+extern crate regex;
 use std::env;
 use clap::{App, Arg};
 use std::fs;
 use std::path::Path;
-use std::process::Command;
+use std::process::{Command, Stdio};
 use execute::Execute;
+use regex::Regex;
+
 
 fn main() {
 
@@ -39,7 +42,8 @@ fn main() {
     let temp_path = Path::new("temp");
 
     if temp_path.exists(){
-        fs::remove_dir_all(temp_path).ok();
+        fs::remove_dir_all(temp_path).ok().expect("Can't remove temp folder");
+        fs::create_dir_all("temp").ok().expect("Can't create a temp folder");
     }else {
         fs::create_dir_all("temp").ok().expect("Can't create a temp folder");
     }
@@ -48,8 +52,8 @@ fn main() {
     let target_quality: f32 = _matches.value_of("TARGET").unwrap_or("4.3").parse().unwrap();
 
     // printing some stuff
-    println!("Using input file {}", input_file);
-    println!("Using target quality {}", target_quality);
+    println!(":: Using input file {}", input_file);
+    println!(":: Using target quality {}", target_quality);
 
 
     // making wav
@@ -69,52 +73,42 @@ fn main() {
         count += 1;
 
         if count > 6{
-            println!("Get more than {}, ending comparison", count );
+            println!(":: Get more than {}, ending comparison", count );
             break
         }
+        let score:f32 = make_probe(bitrate);
 
-        println!("Try: {} Bitrate: {}", count, bitrate);
-
-        if count == 1{
-            let score:f32 = make_probe(bitrate);
-        }
+        println!(":: Try: {} Bitrate: {}, Score: {}", count, bitrate, score);
         break
     }
 }
 
 fn make_probe(bitrate:u32) -> f32{
-    let viqol_score:f32 = 0.9;
 
     // Audio to opus
     let mut cmd = Command::new("ffmpeg");
     cmd.args(&["-y", "-i", "temp/ref.wav", "-c:a","libopus", "-b:a", &format!("{}K", &bitrate.to_string()), &format!("temp/{}.opus", bitrate) ]);
-
-    if let Some(exit_code) = cmd.execute().unwrap() {
-        if exit_code == 0 {}
-        else
-        {
-            eprintln!("Failed");
-            println!("{:?}", cmd.output().unwrap());
-        }
-    }
-    else {eprintln!("Interupted")}
+    cmd.execute().unwrap();
 
     // Audio to wav
     let mut cmd = Command::new("ffmpeg");
-    cmd.args(&["-y", "-i", &format!("temp/{}", bitrate), "-ar", "48000", &format!("temp/{}.wav", bitrate)]);
+    cmd.args(&["-y", "-i", &format!("temp/{}.opus", bitrate), "-ar", "48000", &format!("temp/{}.wav", bitrate)]);
+    cmd.output().expect("can't  convert opus");
 
-    if let Some(exit_code) = cmd.execute().unwrap() {
-        if exit_code == 0 {}
-        else
-        {
-            eprintln!("Failed");
-            println!("{:?}", cmd.output().unwrap());
-        }
-    }
-    else {eprintln!("Interupted")}
+    // calculating score
+    let mut cmd = Command::new("visqol");
+    cmd.args(&["--reference_file", "temp/ref.wav", "--degraded_file", &format!["temp/{}.wav", bitrate]]);
 
-    println!("Calculating Score...");
+    cmd.stdout(Stdio::piped());
+    cmd.stderr(Stdio::piped());
 
+    let output = cmd.execute_output().unwrap();
+    let re = Regex::new(r"([0-9]*\.[0-9]*)": &str).unwrap();
+    let score_str: &str = &String::from_utf8(output.stdout).unwrap();
+
+    let caps= re.captures(&score_str).unwrap();
+    let str_score: &str = caps.get(1).map_or("", |m| m.as_str());
+    let viqol_score:f32 = str_score.parse::<f32>().unwrap();
     viqol_score
 }
 
@@ -135,9 +129,8 @@ fn make_wav(input: &str){
         // Making wav
         let mut cmd = Command::new("ffmpeg");
         cmd.args(&["-y", "-i", input, "-ar", "48000", "temp/ref.wav"]);
-        println!("{:?}", cmd);
         if let Some(exit_code) = cmd.execute().unwrap() {
-            if exit_code == 0{println!("Converted to wav");}
+            if exit_code == 0{}
             else
             {
                 eprintln!("Failed");
