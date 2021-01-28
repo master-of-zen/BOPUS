aa30238d06fe642a421d35b5b43b66b86bf4948f
Yusuf Redzic <yusufredzic@gmail.com>
2021-01-27 18:29:08 -0600

    Only create model if --model isn't specified

--- src/main.rs
+++ src/main.rs
@@ -36,9 +36,9 @@ struct Args {
     #[structopt(short, long = "log", default_value = "INFO")]
     log_level: LevelFilter,
 
-    /// Model to use for visqol calculations
+    /// Model to use for visqol calculations. If not specified, the default model is used
     #[structopt(short, long)]
-    model: PathBuf,
+    model: Option<PathBuf>,
 
     /// Keep temporary folder
     #[structopt(long)]
@@ -102,19 +102,14 @@ fn main() -> anyhow::Result<()> {
 
     // Create all required temp dirs
     create_all_dirs()?;
-    
-    // Create model file
-    create_model()?;
-  
+
+    let model: &Path = match args.model {
+        Some(ref model) => model.as_path(),
+        None => create_model()?,
+    };
+
     info!("Input file: {:?}", args.input);
     info!("Target quality: {:.2}", args.target_quality);
-
-
-
-    // printing some stuff
-    println!(":: Using input file {:?}", args.input);
-    println!(":: Using target quality {}", args.target_quality);
-
 
     // making wav and segmenting
     let wav_segments = segment(&args.input);
@@ -134,7 +129,7 @@ fn main() -> anyhow::Result<()> {
         .build_global()?;
 
     it.par_iter()
-        .for_each(|x| optimize(x, args.target_quality, &args.model));
+        .for_each(|x| optimize(x, args.target_quality, model));
 
     concatenate(&args.input)?;
 
@@ -165,15 +160,14 @@ fn create_all_dirs() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn create_model() -> anyhow::Result<()>{
+fn create_model<'a>() -> anyhow::Result<&'a Path> {
     // Writes included model to temp folder
-    static MODEL: &'static str = include_str!("./visqol_model.txt");
+    const MODEL: &[u8] = include_bytes!("models/visqol_model.txt");
     let model_file = Path::new("temp/model.txt");
     let mut file = File::create(model_file)?;
-    file.write_all(MODEL.as_bytes())?;
-    Ok(())
+    file.write_all(MODEL)?;
+    Ok(model_file)
 }
-
 
 fn concatenate(output: &Path) -> anyhow::Result<()> {
     info!("Concatenating");
