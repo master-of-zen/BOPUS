753da96d65dfed8a7ce5b5f83a3a2373497ab801
redzic <yusufredzic@gmail.com>
2021-02-08 14:19:57 -0600

    Use more idioms, more error handling with anyhow::Result, other stuff

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 use std::cmp;
-use std::ffi::{OsStr, OsString};
+use std::ffi::OsStr;
 use std::fs::{self, DirEntry, File};
 use std::io::prelude::*;
 use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
 
+use anyhow::*;
+
 use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 use regex::Regex;
 use structopt::StructOpt;
+
+use lazy_static::lazy_static;
+
+mod util;
+
+use crate::util::{get_audio_time, transform_score};
 
 #[macro_use]
 extern crate log;
@@ -21,7 +29,7 @@ const SUPPORTED_AUDIO_FORMAT_EXTENSIONS: &[&str] =
 #[structopt(author)]
 struct Args {
     /// Input file to use
-    #[structopt(short, long)]
+    #[structopt(parse(from_os_str), short, long)]
     input: PathBuf,
 
     /// Value of quality to target
@@ -37,19 +45,23 @@ struct Args {
     log_level: LevelFilter,
 
     /// Model to use for visqol calculations. If not specified, the default model is used
-    #[structopt(short, long)]
+    #[structopt(parse(from_os_str), short, long)]
     model: Option<PathBuf>,
 
     /// Keep temporary folder
     #[structopt(long)]
     keep: bool,
+
+    /// Output file
+    #[structopt(name = "OUTPUT", parse(from_os_str))]
+    output: PathBuf,
 }
 
 fn is_program_in_path(program: &str) -> bool {
     which::which(program).is_ok()
 }
 
-fn main() -> anyhow::Result<()> {
+fn main() -> Result<()> {
     let args = Args::from_args();
 
     TermLogger::init(
@@ -66,6 +78,12 @@ fn main() -> anyhow::Result<()> {
         error!("FFmpeg is not installed or in PATH, required for encoding audio");
         return Ok(());
     }
+
+    // uncomment this if get_audio_time function is used
+    // if !is_program_in_path("ffprobe") {
+    //     error!("ffprobe is not installed or in PATH, required for getting metadata from audio files");
+    //     return Ok(());
+    // }
 
     if !is_program_in_path("visqol") {
         error!("visqol is not installed or in PATH, required for perceptual quality metrics");
@@ -111,13 +129,7 @@ fn main() -> anyhow::Result<()> {
     info!("Input file: {:?}", args.input);
     info!("Target quality: {:.2}", args.target_quality);
 
-    // making wav and segmenting
-    let wav_segments = segment(&args.input);
-    let mut it = vec![];
-
-    for seg in wav_segments {
-        it.push(seg?)
-    }
+    let it = segment(&args.input)?;
 
     let jobs = cmp::min(args.jobs.unwrap_or_else(|| num_cpus::get() / 2), it.len());
 
@@ -129,28 +141,16 @@ fn main() -> anyhow::Result<()> {
         .build_global()?;
 
     it.par_iter()
-        .for_each(|x| optimize(x, args.target_quality, model));
+        .map(|x| optimize(x, args.target_quality, model))
+        .collect::<Result<_, _>>()?;
 
-    concatenate(&args.input)?;
+    concatenate(&args.output)?;
 
     if !args.keep {
         fs::remove_dir_all("temp")?;
     }
 
     Ok(())
-}
-
-fn get_audio_time(input: &Path) {
-    // FIXME: Don't allow to segment be less that 5 sec
-    let mut cmd = Command::new("ffmpeg");
-    cmd.args(&["-y", "-i"]);
-    cmd.arg(input);
-
-    cmd.stdout(Stdio::piped());
-    cmd.stderr(Stdio::piped());
-
-    let output = cmd.output().unwrap();
-    debug!("{:?}", output);
 }
 
 macro_rules! remove_and_create {
@@ -163,7 +163,7 @@ macro_rules! remove_and_create {
     }};
 }
 
-fn create_all_dirs() -> anyhow::Result<()> {
+fn create_all_dirs() -> Result<()> {
     // creating temp dir/removing old ones if they exist
     remove_and_create!("temp");
     remove_and_create!("temp/segments");
@@ -173,7 +173,7 @@ fn create_all_dirs() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn create_model<'a>() -> anyhow::Result<&'a Path> {
+fn create_model<'a>() -> Result<&'a Path> {
     // Writes included model to temp folder
     const MODEL: &[u8] = include_bytes!("models/visqol_model.txt");
     let model_file = Path::new("temp/model.txt");
@@ -182,26 +182,7 @@ fn create_model<'a>() -> anyhow::Result<&'a Path> {
     Ok(model_file)
 }
 
-// copied from Rust std
-fn os_str_as_u8_slice(s: &OsStr) -> &[u8] {
-    unsafe { &*(s as *const OsStr as *const [u8]) }
-}
-
-fn u8_slice_as_os_str(s: &[u8]) -> &OsStr {
-    // SAFETY: see the comment of `os_str_as_u8_slice`
-    unsafe { &*(s as *const [u8] as *const OsStr) }
-}
-
-// // Ideally, concat.txt on Windows would be created with UTF-16 encoding
-// #[cfg(not(unix))]
-// fn os_str_to_byte_slice(s: &OsStr) -> &[u8] {
-//     s.to_owned()
-//         .into_string()
-//         .expect("Unsupported conversion from OsString to byte array on non-Unix platforms")
-//         .as_bytes()
-// }
-
-fn concatenate(output: &Path) -> anyhow::Result<()> {
+fn concatenate(output: &Path) -> Result<()> {
     // FIXME: Fix audio silence on concat
     info!("Concatenating");
     let conc_file = Path::new("temp/concat.txt");
@@ -216,11 +197,22 @@ fn concatenate(output: &Path) -> anyhow::Result<()> {
     for p in t {
         // FIXME if the filename contains the ' character, does ffmpeg properly handle it?
         txt.extend_from_slice(b"file 'conc/");
-        txt.extend_from_slice(os_str_as_u8_slice(&p.file_name()));
+        txt.extend_from_slice({
+            #[cfg(unix)]
+            {
+                use std::os::unix::prelude::OsStrExt;
+                &p.file_name().as_bytes()
+            }
+            #[cfg(not(unix))]
+            {
+                &p.file_name()
+                    .into_string()
+                    .expect("non UTF-8 filenames are not supported on non-Unix platforms")
+                    .as_bytes()
+            }
+        });
         txt.extend_from_slice(b"'\n");
     }
-
-    let pt = Path::new(output).with_extension("opus");
 
     let mut file = File::create(conc_file)?;
     file.write_all(txt.as_slice())?;
@@ -229,14 +221,14 @@ fn concatenate(output: &Path) -> anyhow::Result<()> {
     cmd.args(&["-y", "-safe", "0", "-f", "concat", "-i"]);
     cmd.arg(conc_file);
     cmd.args(&["-c", "copy"]);
-    cmd.arg(&pt);
+    cmd.arg(output);
 
     cmd.output()?;
 
     Ok(())
 }
 
-fn optimize(file: &DirEntry, target_quality: f32, model: &Path) {
+fn optimize(file: &DirEntry, target_quality: f32, model: &Path) -> Result<()> {
     const TOLERANCE: f32 = 0.2;
 
     // get metric score
@@ -261,7 +253,7 @@ fn optimize(file: &DirEntry, target_quality: f32, model: &Path) {
         }
 
         let pf = file.path();
-        score = transform_score(make_probe(&pf, bitrate, model));
+        score = transform_score(make_probe(&pf, bitrate, model)?);
         bitrates.push((bitrate, score));
 
         let dif: f32 = (score - target_quality).abs();
@@ -294,19 +286,18 @@ fn optimize(file: &DirEntry, target_quality: f32, model: &Path) {
         .collect::<PathBuf>(),
     );
 
-    cmd.output().unwrap();
+    cmd.output()?;
+
+    Ok(())
 }
 
-fn segment(input: &Path) -> Vec<Result<DirEntry, std::io::Error>> {
+fn segment(input: &Path) -> Result<Vec<DirEntry>, std::io::Error> {
     // FIXME: Don't allow to segment be less that 5 sec
     let segments = Path::new("temp/segments");
     let mut cmd = Command::new("ffmpeg");
+    cmd.args(&["-y", "-i"]);
+    cmd.arg(input);
     cmd.args(&[
-        "-y",
-        "-i",
-        // TODO check if filename can be written as a UTF-8 string, or escape
-        // the chars when passing to ffmpeg when it isn't
-        input.to_str().expect("Filename is not valid UTF-8"),
         "-ar",
         "48000",
         "-f",
@@ -315,119 +306,68 @@ fn segment(input: &Path) -> Vec<Result<DirEntry, std::io::Error>> {
         "12",
         "temp/segments/%05d.wav",
     ]);
-    cmd.output().unwrap();
 
-    fs::read_dir(&segments).unwrap().collect()
+    cmd.output()?;
+
+    fs::read_dir(&segments)?.collect()
 }
 
-/// Transform score for easier score comprehension and usage
-/// Scaled 4.0 - 4.75 range to 0.0 - 5.0
-fn transform_score(score: f32) -> f32 {
-    const SCALE_VALUE: f32 = 5.0 / (4.75 - 4.0);
-
-    if score < 4.1 {
-        1.0f32
-    } else {
-        (score - 4.1) * SCALE_VALUE
-    }
+// lazily compile regex, avoid recompiling the same thing multiple times throughout the program
+lazy_static! {
+    static ref RE: Regex = Regex::new(r"([0-9]*\.[0-9]*)").unwrap();
 }
 
-fn make_probe(file: &Path, bitrate: u32, model: &Path) -> f32 {
+fn make_probe(file: &Path, bitrate: u32, model: &Path) -> Result<f32> {
+    let probe_name = file
+        .file_stem()
+        .unwrap_or_else(|| file.as_os_str())
+        .to_str()
+        // ffmpeg files should not be invalid UTF-8
+        .ok_or(anyhow!("`probe_name` has a non UTF-8 filename"))?;
+
     // Audio to opus
-    let probe_name = file.file_stem().unwrap_or(file.as_os_str());
-
     let mut cmd = Command::new("ffmpeg");
     cmd.args(&["-y", "-i"]);
     cmd.arg(file);
-    cmd.args(&["-c:a", "libopus", "-b:a", &format!("{}", bitrate)]);
-    cmd.arg(
-        &[
-            OsStr::new("temp"),
-            OsStr::new("probes"),
-            u8_slice_as_os_str(
-                vec![
-                    os_str_as_u8_slice(probe_name),
-                    b"_",
-                    bitrate.to_string().as_bytes(),
-                    b".opus",
-                ]
-                .into_iter()
-                .flatten()
-                .map(|byte| *byte)
-                .collect::<Vec<u8>>()
-                .as_ref(),
-            ),
-        ]
-        .iter()
-        .collect::<PathBuf>(),
-    );
-    cmd.output().unwrap();
+    cmd.args(&[
+        "-c:a",
+        "libopus",
+        "-b:a",
+        &format!("{}", bitrate),
+        &format!("temp/probes/{}_{}.opus", probe_name, bitrate),
+    ]);
+    cmd.output()?;
 
     // Audio to wav
     let mut cmd = Command::new("ffmpeg");
-    cmd.args(&[
-        "-y", "-i",
-        // &format!("temp/probes/{}_{}.opus", probe_name, bitrate),
-    ]);
-    // cmd.arg(
-    //     &[
-    //         OsStr::new("temp"),
-    //         OsStr::new("probes"),
-    //         OsStr::new(
-    //             vec![
-    //                 os_str_to_byte_slice(probe_name),
-    //                 b"_",
-    //                 bitrate.to_string().as_bytes(),
-    //                 b".opus",
-    //             ]
-    //             .into_iter()
-    //             .flatten()
-    //             .collect::<Vec<&u8>>()
-    //             .as_ref(),
-    //         ),
-    //     ]
-    //     .iter()
-    //     .collect::<PathBuf>(),
-    // );
-    cmd.args(&[
-        "-ar",
-        "48000",
-        &format!(
-            "temp/probes/{}_{}.wav",
-            // FIX ME
-            probe_name.to_str().unwrap(),
-            bitrate
-        ),
-    ]);
+    cmd.args(&["-y", "-i"]);
+    cmd.arg(&format!("temp/probes/{}_{}.opus", probe_name, bitrate));
+    cmd.args(&["-ar", "48000"]);
+    cmd.arg(&format!("temp/probes/{}_{}.wav", probe_name, bitrate));
     cmd.output().expect("opus encoding failed");
 
     // calculating score
     let mut cmd = Command::new("visqol");
 
-    cmd.args(&["--similarity_to_quality_model"]);
+    cmd.arg("--similarity_to_quality_model");
     cmd.arg(model);
-    cmd.args(&[
-        "--reference_file",
-        file.to_str().unwrap(),
-        "--degraded_file",
-        &format!(
-            "temp/probes/{}_{}.wav",
-            probe_name.to_str().unwrap(),
-            bitrate
-        ),
-    ]);
+    cmd.arg("--reference_file");
+    cmd.arg(file);
+    cmd.arg("--degraded_file");
+    cmd.arg(&format!("temp/probes/{}_{}.wav", probe_name, bitrate));
 
     cmd.stdout(Stdio::piped());
     cmd.stderr(Stdio::piped());
 
-    let output = cmd.output().unwrap();
+    let output = cmd.output()?;
     debug!("{:?}", output);
 
-    let re = Regex::new(r"([0-9]*\.[0-9]*)").unwrap();
-    let score_str = String::from_utf8(output.stdout).unwrap();
+    let score_str = String::from_utf8(output.stdout)?;
 
-    let caps = re.captures(&score_str).unwrap();
+    let caps = RE
+        .captures(&score_str)
+        .ok_or(anyhow!("regex capture failed"))?;
     let score: &str = caps.get(1).map_or("", |m| m.as_str());
 
-    score.parse().unwrap()
+    Ok(score.parse::<f32>()?)
 }
